61d232379891079eb676bc71bd8aa070689676f0
FunkyPancake <matdeb01685@gmail.com>
2023-02-26 22:42:43 +0100

    Add simple read and write memory commands for calibration

--- Utils/Calibration/Calibration.cpp
+++ Utils/Calibration/Calibration.cpp
@@ -1,90 +1,125 @@
 #include "Calibration.h"
+#include <cstring>
 
 using Utils::Calibration;
 Calibration::Calibration()
-    : _commandMapping{
-        {Calibration::Commands::Connect, &Calibration::ProcessConnect},
-                {Calibration::Commands::Disconnect, &Calibration::ProcessDisconnect},
-                {Calibration::Commands::Reset, &Calibration::ProcessReset},
-                {Calibration::Commands::GetControlBlock, &Calibration::ProcessGetControlBlock},
-                {Calibration::Commands::ReadMemory, &Calibration::ProcessReadMemory},
-                {Calibration::Commands::WriteMemory, &Calibration::ProcessWriteMemory},
-                {Calibration::Commands::Program, &Calibration::ProcessProgram},
-                {Calibration::Commands::ConfigureCyclicRead, &Calibration::ProcessConfigureCyclicRead},
-                {Calibration::Commands::StartCyclicRead, &Calibration::ProcessStartCyclicRead},
-                {Calibration::Commands::StopCyclicRead, &Calibration::ProcessStopCyclicRead},
-                {Calibration::Commands::ClearCyclicRead, &Calibration::ProcessClearCyclicRead},
-                {Calibration::Commands::JumpToFbl, &Calibration::ProcessJumpToFbl},
-                {Calibration::Commands::EraseNvm, &Calibration::ProcessEraseNvm},
-                {Calibration::Commands::Sync, &Calibration::ProcessSync}
-    }
+    : responseBuffer(BufferLength),
+      _commandMapping{
+          {Calibration::Commands::Connect, &Calibration::ProcessConnect},
+          {Calibration::Commands::Disconnect, &Calibration::ProcessDisconnect},
+          {Calibration::Commands::Reset, &Calibration::ProcessReset},
+          {Calibration::Commands::GetControlBlock, &Calibration::ProcessGetControlBlock},
+          {Calibration::Commands::ReadMemory, &Calibration::ProcessReadMemory},
+          {Calibration::Commands::WriteMemory, &Calibration::ProcessWriteMemory},
+          {Calibration::Commands::Program, &Calibration::ProcessProgram},
+          {Calibration::Commands::ConfigureCyclicRead, &Calibration::ProcessConfigureCyclicRead},
+          {Calibration::Commands::StartCyclicRead, &Calibration::ProcessStartCyclicRead},
+          {Calibration::Commands::StopCyclicRead, &Calibration::ProcessStopCyclicRead},
+          {Calibration::Commands::ClearCyclicRead, &Calibration::ProcessClearCyclicRead},
+          {Calibration::Commands::JumpToFbl, &Calibration::ProcessJumpToFbl},
+          {Calibration::Commands::EraseNvm, &Calibration::ProcessEraseNvm},
+          {Calibration::Commands::Sync, &Calibration::ProcessSync}}
 {
 }
 
-std::vector<uint8_t> Calibration::ProcessConnect(std::vector<uint8_t> &command)
+void Calibration::ProcessConnect(std::vector<uint8_t> &command)
 {
-    return {};
+
+    ErrorHandler();
 }
-std::vector<uint8_t> Calibration::ProcessCommand(std::vector<uint8_t> &command)
+std::vector<uint8_t> &Calibration::ProcessCommand(std::vector<uint8_t> &command)
 {
-    if(_commandMapping.find(static_cast<Calibration::Commands>(command[0])) == _commandMapping.end()){
+    auto iter = _commandMapping.find(static_cast<Calibration::Commands>(command[0]));
+    if (iter == _commandMapping.end()) {
         ErrorHandler();
     }
-
-    return {};
+    else {
+        (*this.*(iter->second))(command);
+    }
+    return responseBuffer;
 }
 void Calibration::ErrorHandler()
 {
+    responseBuffer.resize(1);
+    responseBuffer[0] = static_cast<uint8_t>(Resposnse::Negative);
 }
-std::vector<uint8_t> Utils::Calibration::ProcessDisconnect(std::vector<uint8_t> &command)
+void Utils::Calibration::ProcessDisconnect(std::vector<uint8_t> &command)
 {
-    return std::vector<uint8_t>();
+    ErrorHandler();
 }
-std::vector<uint8_t> Utils::Calibration::ProcessReset(std::vector<uint8_t> &command)
+void Utils::Calibration::ProcessReset(std::vector<uint8_t> &command)
 {
-    return std::vector<uint8_t>();
+    ErrorHandler();
 }
-std::vector<uint8_t> Utils::Calibration::ProcessGetControlBlock(std::vector<uint8_t> &command)
+void Utils::Calibration::ProcessGetControlBlock(std::vector<uint8_t> &command)
 {
-    return std::vector<uint8_t>();
+    ErrorHandler();
 }
-std::vector<uint8_t> Utils::Calibration::ProcessReadMemory(std::vector<uint8_t> &command)
+
+void Utils::Calibration::ProcessReadMemory(std::vector<uint8_t> &command)
 {
-    return std::vector<uint8_t>();
+    if (command.size() != 7)
+    {
+        ErrorHandler();
+        return;
+    }
+    uintptr_t address;
+    uint16_t length;
+    std::memcpy(&address, command.data() + 1, sizeof(uint8_t *));
+    std::memcpy(&length, command.data() + 5, sizeof(uint16_t));
+    if(length > BufferLength-1){
+        ErrorHandler();
+        return;
+    }
+    responseBuffer.resize(length + 1);
+    responseBuffer[0] = static_cast<uint8_t>(Resposnse::Positive);
+    std::memcpy(responseBuffer.data() + 1, reinterpret_cast<uint8_t *>(address), length);
 }
-std::vector<uint8_t> Utils::Calibration::ProcessWriteMemory(std::vector<uint8_t> &command)
+
+void Utils::Calibration::ProcessWriteMemory(std::vector<uint8_t> &command)
 {
-    return std::vector<uint8_t>();
+    if (command.size() < 8)
+    {
+        ErrorHandler();
+        return;
+    }
+    uintptr_t address;
+    uint16_t length;
+    std::memcpy(&address, command.data() + 1, sizeof(uint8_t *));
+    std::memcpy(&length, command.data() + 5, sizeof(uint16_t));
+    std::memcpy(reinterpret_cast<uint8_t *>(address),command.data() + 7,length);
+    responseBuffer.resize(1);
+    responseBuffer[0] = static_cast<uint8_t>(Resposnse::Positive);
 }
-std::vector<uint8_t> Utils::Calibration::ProcessProgram(std::vector<uint8_t> &command)
+void Utils::Calibration::ProcessProgram(std::vector<uint8_t> &command)
 {
-    return std::vector<uint8_t>();
+    ErrorHandler();
 }
-std::vector<uint8_t> Utils::Calibration::ProcessConfigureCyclicRead(std::vector<uint8_t> &command)
+void Utils::Calibration::ProcessConfigureCyclicRead(std::vector<uint8_t> &command)
 {
-    return std::vector<uint8_t>();
+    ErrorHandler();
 }
-std::vector<uint8_t> Utils::Calibration::ProcessClearCyclicRead(std::vector<uint8_t> &command)
+void Utils::Calibration::ProcessClearCyclicRead(std::vector<uint8_t> &command)
 {
-    return std::vector<uint8_t>();
+    ErrorHandler();
 }
-std::vector<uint8_t> Utils::Calibration::ProcessJumpToFbl(std::vector<uint8_t> &command)
+void Utils::Calibration::ProcessJumpToFbl(std::vector<uint8_t> &command)
 {
-    return std::vector<uint8_t>();
+    ErrorHandler();
 }
-std::vector<uint8_t> Utils::Calibration::ProcessEraseNvm(std::vector<uint8_t> &command)
+void Utils::Calibration::ProcessEraseNvm(std::vector<uint8_t> &command)
 {
-    return std::vector<uint8_t>();
+    ErrorHandler();
 }
-std::vector<uint8_t> Utils::Calibration::ProcessStartCyclicRead(std::vector<uint8_t> &command)
+void Utils::Calibration::ProcessStartCyclicRead(std::vector<uint8_t> &command)
 {
-    return std::vector<uint8_t>();
+    ErrorHandler();
 }
-std::vector<uint8_t> Utils::Calibration::ProcessStopCyclicRead(std::vector<uint8_t> &command)
+void Utils::Calibration::ProcessStopCyclicRead(std::vector<uint8_t> &command)
 {
-    return std::vector<uint8_t>();
+    ErrorHandler();
 }
-std::vector<uint8_t> Utils::Calibration::ProcessSync(std::vector<uint8_t> &command)
+void Utils::Calibration::ProcessSync(std::vector<uint8_t> &command)
 {
-    return std::vector<uint8_t>();
+    ErrorHandler();
 }
--- Utils/Calibration/Calibration.h
+++ Utils/Calibration/Calibration.h
@@ -9,44 +9,49 @@ namespace Utils
     class Calibration
     {
     private:
+        inline static constexpr uint16_t BufferLength=256;
         std::vector<uint8_t> responseBuffer;
+        enum class Resposnse{
+            Positive = 0xFF,
+            Negative = 0xFE
+        };
         enum class Commands
         {
-            Connect,
-            Disconnect,
+            Connect = 0xff,
+            Disconnect =0xfe,
+            Sync = 0xfc,
             Reset,
             GetControlBlock,
-            ReadMemory,
-            WriteMemory,
+            ReadMemory = 0xf5,
+            WriteMemory=0xf0,
             Program,
             ConfigureCyclicRead,
             StartCyclicRead,
             StopCyclicRead,
             ClearCyclicRead,
             JumpToFbl,
-            Sync,
             EraseNvm
         };
-        typedef std::vector<uint8_t> (Calibration::*CommandProcessor)(std::vector<uint8_t> &command);
-        std::vector<uint8_t> ProcessDisconnect(std::vector<uint8_t> &command);
-        std::vector<uint8_t> ProcessReset(std::vector<uint8_t> &command);
-        std::vector<uint8_t> ProcessGetControlBlock(std::vector<uint8_t> &command);
-        std::vector<uint8_t> ProcessReadMemory(std::vector<uint8_t> &command);
-        std::vector<uint8_t> ProcessWriteMemory(std::vector<uint8_t> &command);
-        std::vector<uint8_t> ProcessProgram(std::vector<uint8_t> &command);
-        std::vector<uint8_t> ProcessConfigureCyclicRead(std::vector<uint8_t> &command);
-        std::vector<uint8_t> ProcessClearCyclicRead(std::vector<uint8_t> &command);
-        std::vector<uint8_t> ProcessJumpToFbl(std::vector<uint8_t> &command);
-        std::vector<uint8_t> ProcessEraseNvm(std::vector<uint8_t> &command);
-        std::vector<uint8_t> ProcessStartCyclicRead(std::vector<uint8_t> &command);
-        std::vector<uint8_t> ProcessStopCyclicRead(std::vector<uint8_t> &command);
-        std::vector<uint8_t> ProcessConnect(std::vector<uint8_t> &command);
-        std::vector<uint8_t> ProcessSync(std::vector<uint8_t> &command);
+        typedef void (Calibration::*CommandProcessor)(std::vector<uint8_t> &command);
         const std::unordered_map<Commands, CommandProcessor> _commandMapping;
+        void ProcessDisconnect(std::vector<uint8_t> &command);
+        void ProcessReset(std::vector<uint8_t> &command);
+        void ProcessGetControlBlock(std::vector<uint8_t> &command);
+        void ProcessReadMemory(std::vector<uint8_t> &command);
+        void ProcessWriteMemory(std::vector<uint8_t> &command);
+        void ProcessProgram(std::vector<uint8_t> &command);
+        void ProcessConfigureCyclicRead(std::vector<uint8_t> &command);
+        void ProcessClearCyclicRead(std::vector<uint8_t> &command);
+        void ProcessJumpToFbl(std::vector<uint8_t> &command);
+        void ProcessEraseNvm(std::vector<uint8_t> &command);
+        void ProcessStartCyclicRead(std::vector<uint8_t> &command);
+        void ProcessStopCyclicRead(std::vector<uint8_t> &command);
+        void ProcessConnect(std::vector<uint8_t> &command);
+        void ProcessSync(std::vector<uint8_t> &command);
         void ErrorHandler();
 
     public:
-        std::vector<uint8_t> ProcessCommand(std::vector<uint8_t> &command);
+        std::vector<uint8_t>& ProcessCommand(std::vector<uint8_t> &command);
         Calibration();
     };
 }// namespace Utils
