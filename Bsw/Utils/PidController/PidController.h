12dd6c18a081b39c5fae362bd3640a9c277fbd75
FunkyPancake <43291121+FunkyPancake@users.noreply.github.com>
2023-11-23 16:10:08 +0100

    remove template, use only floats, add a task for position/speed loop and more functionality to motor controller

--- Bsw/Utils/PidController/PidController.h
+++ Bsw/Utils/PidController/PidController.h
@@ -9,60 +9,62 @@
 #define PID_CONTROLLER_H_
 #include <type_traits>
 
-namespace KeCommon::Utils {
-	template <class T> class PidController
-	{
-	private:
-		T p,i,d,iterm;
-	public:
-		explicit PidController()
-		{
-			static_assert(std::is_floating_point_v<T> || std::is_integral_v<T>);
+namespace KeCommon::Utils
+{
+    struct PidConfig
+    {
+        float Kp, Ki, Kd, ItermLimitHigh, ItermLimitLow, OutputMax, OutputMin, ItermActivationThreshold, DeadBand;
+    };
 
-		}
-		void Reset()
-		{
-			iterm = 0;
-		}
-		void SetTarget(const T& target);
-		T ControlLoop(const T& input);
-	};
+    class PidController
+    {
+        float _errorLast{0}, _iterm{0}, _target{0}, _lastOutput{0};
+        PidConfig _config;
+
+        static void Limit(float &value, const float &limitHigh, const float &limitLow)
+        {
+            if (value > limitHigh) {
+                value = limitHigh;
+            }
+            if (value < limitLow) {
+                value = limitLow;
+            }
+        }
+
+    public:
+        explicit PidController(const PidConfig &config)
+            : _config{config}
+        {
+            // static_assert(std::is_floating_point_v<T> || std::is_integral_v<T>);
+        }
+
+        void SetTarget(const float &target)
+        {
+            _target = target;
+        }
+
+        float ControlLoop(const float &input)
+        {
+            const auto error = _target - input;
+            if (error < _config.DeadBand) {
+                return _lastOutput;
+            }
+
+            if (error < _config.ItermActivationThreshold) {
+                _iterm += error;
+                Limit(_iterm, _config.ItermLimitHigh, _config.ItermLimitLow);
+            }
+            else {
+                _iterm = 0;
+            }
+            const auto errorDelta = error - _errorLast;
+            auto output = error * _config.Kp + _iterm * _config.Ki + errorDelta * _config.Kd;
+            Limit(output, _config.OutputMax, _config.OutputMin);
+            _errorLast = error;
+            _lastOutput = output;
+            return output;
+        }
+    };
 }
-// typedef struct
-// {
-// 	real_t p;
-// 	real_t i;
-// 	real_t d;
-// 	real_t iterm_sat_high;
-// 	real_t iterm_sat_low;
-// 	real_t out_sat_high;
-// 	real_t out_sat_low;
-// } PidCal_f_t;
-// typedef struct
-// {
-// 	real_t last_error;
-// 	real_t iterm;
-// } PidData_f_t;
-//
-// typedef struct
-// {
-// 	sint16 p;
-// 	sint16 i;
-// 	sint16 d;
-// 	sint16 scale;
-// 	int32_t iterm_sat_high;
-// 	int32_t iterm_sat_low;
-// 	sint16 out_sat_high;
-// 	sint16 out_sat_low;
-// } PidCal_i16_t;
-// typedef struct
-// {
-// 	int32_t last_error;
-// 	int32_t iterm;
-// } PidData_i16_t;
-//
-// real_t PidControler_f(real_t trg, real_t proces, PidData_f_t* pid_data, const PidCal_f_t* pid_cal);
-// sint16 PidControler_i16(sint16 trg,sint16 proces, PidData_i16_t* pid_data, const PidCal_i16_t* pid_cal);
-// void PidClear_f(PidData_f_t* pid_data);
 
 #endif /* PID_CONTROLLER_H_ */
